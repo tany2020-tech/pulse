f9a386977029f4068044bf31e194536a36da998b
Tany <example@gmail.com>
2021-03-12 11:52:55 +0200

    12march

--- src/js/script.js
+++ src/js/script.js
@@ -17,15 +17,16 @@ $(document).ready(function(){
                 dots: true
                
                 }
-              },
+              }
         ]
       });
 
 
+
       $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        
+        $(this).addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
 
       // $ ('.catalog-item__link').each(function(i) {
@@ -55,7 +56,7 @@ $(document).ready(function(){
 
                function toggleSlide(item) {
                 $(item).each (function(i){
-                  $(this).on ('click', function(e) {
+                  $(this).on('click', function(e) {
                     e.preventDefault();
                   $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                   $ ('.catalog-item__list') .eq(i).toggleClass('catalog-item__list_active');
@@ -63,6 +64,7 @@ $(document).ready(function(){
                 }) ;
                  });
                }
+
          toggleSlide('.catalog-item__link');
          toggleSlide ('.catalog-item__back');
       
@@ -77,9 +79,7 @@ $(document).ready(function(){
         $('.overlay, #consultation,#thanks, #order').fadeOut('slow');
 
        });
-
-     
-       
+      
 
        $('.catalog-item__btn').each(function(i){
        $(this).on('click',function(){
@@ -90,15 +90,37 @@ $(document).ready(function(){
       });
     
 
-      //  $('.feed-form').validate(); 
-      //  $('#consultation form').validate({
+                                    //  $('.feed-form').validate(); 
+                                    //  $('#consultation form').validate({
+                                    //     rules:{
+                                    //         name: "required",  
+                                    //         phone: "required",
+                                    //         email: {
+                                    //                required: true,
+                                    //                email: true
+
+                                    //         }     
+                                    //         },
+
+                                    //         messages: {
+                                    //           name: "Please specify your name",
+                                    //           email: {
+                                    //             required: "We need your email address to contact you",
+                                    //             email: "Your email address must be in the format of name@domain.com"
+                                    //           }
+                                    //         }
+                                    //     });
+
+                                    //  $('#order form').validate();
+
+      //  function valideForms(form) {
+      //      $(form).validate({
       //     rules:{
       //         name: "required",  
       //         phone: "required",
       //         email: {
       //                required: true,
       //                email: true
-
       //         }     
       //         },
 
@@ -110,40 +132,56 @@ $(document).ready(function(){
       //           }
       //         }
       //     });
+      //  }
 
-      //  $('#order form').validate();
-
-       function valideForms(form) {
-             
-
-        $(form).validate({
-          rules:{
-              name: "required",  
-              phone: "required",
-              email: {
-                     required: true,
-                     email: true
-
-              }     
-              },
-
-              messages: {
-                name: "Please specify your name",
-                email: {
-                  required: "We need your email address to contact you",
-                  email: "Your email address must be in the format of name@domain.com"
-                }
-              }
-          });
-       }
-
-
-       valideForms('#order form');
-       valideForms('.feed-form');
-       valideForms('#consultation form');
-      
-      
+      //   valideForms('#order form');
+      //  valideForms('#consultation form');
+      //   valideForms('#consultation-form');
+         
+             $('form').submit(function(e){
+                e.preventDefault();
+               $.ajax({
+                   type: 'POST',
+                   url: 'mailer/smart.php',
+                   data: $(this).serialize()
+                  }).done(function() {
+                     $(this).find('input').val("");     
+                    $('#consultation, #order').fadeOut();
+                     $('.overlay, #thanks').fadeIn('slow');
+                     $('form').trigger('reset');
+                     
+                });
+          
+               return false;
         
-    });
- 
- 
+               });
+   
+
+          $(window).scroll( function() {
+
+           if($(this).scrollTop() > 1000 ) {
+
+                $('.pageup').fadeIn();
+           }
+           else {
+            $('.pageup').fadeOut();
+           }
+
+
+          })  ;                    
+                            
+    
+
+          
+            $("a[href^='#']").click(function(){
+                    const _href = $(this).attr("href");
+                    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                    return false;
+            });
+   
+
+
+        
+        });
+  
+      
