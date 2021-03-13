a882b7ea1eaf45d94ca9f7959074fe391b4ba516
Tany <example@gmail.com>
2021-03-13 08:57:30 +0200

    13march

--- src/js/script.js
+++ src/js/script.js
@@ -174,7 +174,7 @@ $(document).ready(function(){
 
           
             $("a[href^='#']").click(function(){
-                    const _href = $(this).attr("href");
+                    var _href = $(this).attr("href");
                     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
                     return false;
             });
